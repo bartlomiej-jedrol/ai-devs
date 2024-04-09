fd8ee3c0a4139782a66c418603c7c02ffd3f717a
bartlomiej-jedrol <bartlomiej.jedrol@gmail.com>
2024-04-09 21:07:44 +0200

    modified create completion function

--- lib/openai_utilities.py
+++ lib/openai_utilities.py
@@ -1,17 +1,37 @@
 import logging
-from typing import Optional
+from typing import Optional, Dict, List
 
 logger = logging.getLogger()
 
 
-def create_completion(client, messages, model) -> Optional[str]:
+def create_completion(
+    client: str,
+    messages: Optional[List[Dict[str, str]]],
+    model: str,
+    tools: Dict = None,
+) -> Optional[str]:
     """Call OpenAI's Chat completion API."""
-    try:
-        completion = client.chat.completions.create(
-            messages=messages, model=model, temperature=0
-        )
-        response = completion.model_dump()
-        return response["choices"][0]["message"]["content"]
-    except ValueError as e:
-        logger.error(e)
-        return None
+    if tools is None:
+        try:
+            completion = client.chat.completions.create(
+                messages=messages, model=model, temperature=0
+            )
+            response = completion.model_dump()
+            logger.info(f"Successfully created completion: {response}")
+            return response["choices"][0]["message"]["content"]
+        except ValueError as e:
+            logger.error(e)
+            return None
+    else:
+        try:
+            completion = client.chat.completions.create(
+                messages=messages, model=model, tools=tools, temperature=0
+            )
+            response = completion.model_dump()
+            logger.info(f"Successfully created completion: {response}")
+            return response["choices"][0]["message"]["tool_calls"][0]["function"][
+                "arguments"
+            ]
+        except ValueError as e:
+            logger.error(e)
+            return None
