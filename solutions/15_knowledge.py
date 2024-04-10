5c8df22723318ebb7b58f6fcfc3b43ab67a5234c
bartlomiej-jedrol <bartlomiej.jedrol@gmail.com>
2024-04-10 13:16:41 +0200

    modified function calling

--- solutions/15_knowledge.py
+++ solutions/15_knowledge.py
@@ -1,190 +1,162 @@
 import logging
 import json
-import requests
-from typing import Optional, Tuple, Union, List, Dict
+from typing import Optional, Dict, Any
+from datetime import datetime, timedelta
+from urllib.parse import urljoin
 
 from openai import OpenAI
-import pandas as pd
 
 from lib.handle_task import get_task_token, get_task_details, send_answer
-from lib.utilities import extract_urls, retrieve_json_data_from_url
+from lib.utilities import send_request
 from lib.get_model import get_model
 from lib.openai_utilities import create_completion
 
+logging.basicConfig(
+    filename="app.log",
+    filemode="w",
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+    level=logging.INFO,
+)
 logger = logging.getLogger()
 
 OPENAI_CLIENT = OpenAI()
 MODEL = get_model("gpt4")
-EXCHANGE_RATE_FUNCTION_NAME = "get_exchange_rate"
-COUNTRY_POPULATION_FUNCTION_NAME = "get_country_population"
-OPENAI_FUNCTION_NAME = "get_openai_answer"
+CURRENCY = "currency"
+COUNTRY = "country"
+ANSWER = "answer"
 
 
 def get_function_specification():
-    """Get the function specification for the determine service function."""
+    """Get the function specification for the determine service function (Function calling)."""
     return [
         {
             "type": "function",
             "function": {
                 "name": "determine_service",
-                "description": "determine function name based on user question",
+                "description": "Determine function name and its input based on user question.",
                 "parameters": {
                     "type": "object",
                     "properties": {
-                        "target_function_name": {
+                        "question": {
                             "type": "string",
-                            "description": f"{EXCHANGE_RATE_FUNCTION_NAME}|{COUNTRY_POPULATION_FUNCTION_NAME}|{OPENAI_FUNCTION_NAME}",
+                            "description": "Unchanged user question.",
+                        },
+                        "service_name": {
+                            "type": "string",
+                            "description": f"{CURRENCY} (only for questions related to exchanging rate) | {COUNTRY} (only for questions related to population number) | {ANSWER} (otherwise)",
+                            "enum": [CURRENCY, COUNTRY, ANSWER],
+                        },
+                        CURRENCY: {
+                            "type": "string",
+                            "description": "Currency code in the ISO 4217 standard (three characters)",
+                        },
+                        COUNTRY: {
+                            "type": "string",
+                            "description": "Country name in English",
+                        },
+                        ANSWER: {
+                            "type": "string",
+                            "description": "Answer for user question. Required in service_name is answer.",
                         },
                     },
+                    "required": ["question", "service_name"],
                 },
             },
         }
     ]
 
 
-def determine_target_function_for_question(
-    openai_client: OpenAI, model: str, question: str
-) -> str:
-    """Determine target function to be called for a given question.
-    There are three services possible to be called and for each there is a separate function:
-    - get_exchange_rate
-    - get_country_population
-    - get_openai_answer
+def classify_question(openai_client: str, model: str, question: str) -> Dict[str, Any]:
+    """Classify question using OpenAI model.
+    Return arguments.
     """
     messages = []
-    messages.append(
-        {
-            "role": "system",
-            "content": """Determine function name based on user question. There are three function to be chosen from:
-                    - get_exchange_rate: only for questions related to exchanging rate
-                    - get_country_population: only for questions related to population number
-                    - get_openai_answer: rest of the questions
-                    examples```
-                    question: podaj aktualny kurs DOLARA
-                    target_function_name: get_exchange_rate
-                    question: jak nazywa się stolica Czech?
-                    target_function_name: get_openai_answer
-                    ```
-                    """,
-        }
-    )
+    messages.append({"role": "system", "content": ""})
     messages.append({"role": "user", "content": question})
     tools = get_function_specification()
-
     arguments = create_completion(
-        client=OPENAI_CLIENT, model=MODEL, messages=messages, tools=tools
+        client=openai_client, model=model, messages=messages, tools=tools
     )
-    return json.loads(arguments)["target_function_name"]
+    return json.loads(arguments)
 
 
-def get_exchange_rate(question: str):
+def execute_target_function(arguments: Dict[str, str]) -> Optional[Any]:
+    """
+    Map service name to target function.
+    Execute target function based on provided arguments from the OpeAI model.
+    Return answer based on the target function execution.
+
+    Mapping:
+    - service name -> target function
+    - currency -> get_exchange_rate
+    - country -> get_country_population
+    - answer -> get_openai_answer
+    """
+    service_name = arguments.get("service_name")
+    if service_name == CURRENCY:
+        function_input = arguments[CURRENCY]
+        answer = get_exchange_rate(function_input=function_input)
+    elif service_name == COUNTRY:
+        function_input = arguments[COUNTRY]
+        answer = get_country_population(function_input=function_input)
+    elif service_name == ANSWER:
+        answer = arguments[ANSWER]
+
+    if answer is not None:
+        return answer
+
+
+def get_exchange_rate(function_input: str) -> Optional[float]:
     """Get exchange rate for currency."""
+    base_url = "http://api.nbp.pl/api/exchangerates/tables/"
     table = "A"
+    previous_day_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+    exchange_rate_url = urljoin(base_url, f"{table}/{previous_day_date}/")
 
-    # Extract currency code from question
-    messages = []
-    messages.append(
-        {
-            "role": "system",
-            "content": "Return only currency code in the ISO 4217 standard (three characters).",
-        }
-    )
-    messages.append(
-        {
-            "role": "user",
-            "content": question,
-        }
-    )
-    iso_currency_code = create_completion(
-        client=OPENAI_CLIENT, model=MODEL, messages=messages
-    )
-    print(iso_currency_code)
+    # Send a request to the NBP API
+    response_json = send_request(url=exchange_rate_url)
+    if response_json is None:
+        return None
 
-    echange_rate_url = (
-        f"http://api.nbp.pl/api/exchangerates/rates/{table}/{iso_currency_code}/today/"
-    )
-    response = requests.get(url=echange_rate_url)
-    response_json = response.json()
-    return response_json["rates"][0]["mid"]
+    currency_rate = None
+    for rate in response_json[0]["rates"]:
+        if rate["code"] == function_input:
+            currency_rate = rate["mid"]
+            break
+    return currency_rate
 
 
-def get_country_population(question: str):
+def get_country_population(function_input: str) -> Optional[int]:
     """Get country population for country."""
+    base_url = "https://restcountries.com/v3.1/name/"
+    country_population_url = urljoin(base_url, function_input)
 
-    # Extract country name from question
-    messages = []
-    messages.append(
-        {
-            "role": "system",
-            "content": "Return only country name in English from question.",
-        }
-    )
-    messages.append(
-        {
-            "role": "user",
-            "content": question,
-        }
-    )
-    country_name = create_completion(
-        client=OPENAI_CLIENT, model=MODEL, messages=messages
-    )
-    print(country_name)
+    # Send request to the country population api
+    response_json = send_request(url=country_population_url)
+    if response_json is None:
+        return None
 
-    country_population_url = f"https://restcountries.com/v3.1/name/{country_name}"
-    response = requests.get(url=country_population_url)
-    response_json = response.json()
-    population = response_json[0]["population"]
-    print(population)
+    return response_json[0]["population"]
 
 
-def get_openai_answer(question: str):
-    """Get answer for question."""
-    messages = []
-    messages.append(
-        {
-            "role": "system",
-            "content": """Answer question very concisely.
-                    examples```
-                    question: Who wrote Romeo and Juliet?
-                    response: William Shakespeare
-                    ```
-                    """,
-        }
-    )
-    messages.append(
-        {
-            "role": "user",
-            "content": question,
-        }
-    )
-    return create_completion(client=OPENAI_CLIENT, model=MODEL, messages=messages)
-
-
-def extract_input_for_target_function(question: str) -> str:
-    print("input")
-
-
-def get_answer():
-    """Get answer for a question from ai_devs."""
+def main() -> None:
+    """Get answer for the ai_devs question."""
     ai_devs_task_token = get_task_token("knowledge")
     question = get_task_details(token=ai_devs_task_token)["question"]
-    print(question)
 
-    target_function = determine_target_function_for_question(
+    arguments = classify_question(
         openai_client=OPENAI_CLIENT, model=MODEL, question=question
     )
+    if arguments is None:
+        return None
 
-    if target_function == EXCHANGE_RATE_FUNCTION_NAME:
-        answer = get_exchange_rate(question=question)
-        print(answer)
-    elif target_function == COUNTRY_POPULATION_FUNCTION_NAME:
-        answer = get_country_population(question=question)
-        print(answer)
-    elif target_function == OPENAI_FUNCTION_NAME:
-        answer = get_openai_answer(question=question)
-        print(answer)
+    answer = execute_target_function(arguments=arguments)
+    if answer is None:
+        return None
 
-    print(send_answer(token=ai_devs_task_token, answer=answer))
+    # Send answer to ai_devs server
+    send_answer(token=ai_devs_task_token, answer=answer)
 
 
-get_answer()
+main()
